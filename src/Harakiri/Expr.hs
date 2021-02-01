f5256d6900e806a86f290a18e000605ed7010445
cyberfined <cyberfined@protonmail.com>
2021-02-01 18:37:38 +0300

    Implement AST interpreter

--- src/Harakiri/Expr.hs
+++ src/Harakiri/Expr.hs
@@ -2,8 +2,10 @@ module Harakiri.Expr
     ( module Harakiri.Expr.Types
     , module Harakiri.Expr.Annotated
     , module Harakiri.Expr.Shorthands
+    , module Harakiri.Expr.Interpreter
     ) where
 
 import Harakiri.Expr.Types
 import Harakiri.Expr.Annotated
 import Harakiri.Expr.Shorthands
+import Harakiri.Expr.Interpreter
