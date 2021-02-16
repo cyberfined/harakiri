121cb7994623b087b261ccef8ebf25bb2c60993c
cyberfined <cyberfined@protonmail.com>
2021-02-16 20:51:02 +0300

    Implement Compile module

--- src/Harakiri/Compiler.hs
+++ src/Harakiri/Compiler.hs
@@ -1,5 +1,7 @@
 module Harakiri.Compiler
     ( module Harakiri.Compiler.Architecture
+    , module Harakiri.Compiler.Compile
     ) where
 
 import Harakiri.Compiler.Architecture
+import Harakiri.Compiler.Compile
